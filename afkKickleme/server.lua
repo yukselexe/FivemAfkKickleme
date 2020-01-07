ab7de5c8ef087d56a7407978a50cf77a826a563f
Yüksel Ant Özdemir <48071814+yukselexe@users.noreply.github.com>
2020-01-07 14:38:57 +0300

    function name update
    why not .d

--- afkKickleme/server.lua
+++ afkKickleme/server.lua
@@ -1,5 +1,5 @@
-RegisterServerEvent("kickForBeingAnAFKDouchebag")
-AddEventHandler("kickForBeingAnAFKDouchebag", function()
+RegisterServerEvent("yallahafk")
+AddEventHandler("yallahafk", function()
 	DropPlayer(source, "Uzun süre afk kaldığınız için kicklendiniz.") --kickleme eventi--
 end)
  --yukselexe tarafından yapılmıştır--
