ffa5be5ef3d04c32543dd9f265310d86a280b97b
Yüksel Ant Özdemir <48071814+yukselexe@users.noreply.github.com>
2020-01-07 14:41:25 +0300

    manifest version update :D
    why not

--- afkKickleme/__resource.lua
+++ afkKickleme/__resource.lua
@@ -1,4 +1,4 @@
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+resource_manifest_version 'kfuth8yv-559n-2fght5-8j50-un38h3q9j3ftgz2'
 
 client_script "client.lua" --Client taraflı scripti belirledik
 server_script "server.lua" --Server taraflı scripti belirledik
